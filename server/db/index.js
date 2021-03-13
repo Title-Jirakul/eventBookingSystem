3a28a4c147cd464eec98a942175ac677d7cacf06
Title-Jirakul <titlejirakul@gmail.com>
2021-03-13 00:12:54 -0800

    setting up db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
+// const mongoURL = 'mongodb://127.0.0.1:27017/booking'
+const mongoURL = process.env.MONGODB_URI
+
 mongoose
-    .connect('mongodb://127.0.0.1:27017/booking', { useNewUrlParser: true })
+    .connect(mongoURL, { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
