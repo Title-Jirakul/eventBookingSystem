0953cd9f195bf118aa1703472022799b97789b66
Title-Jirakul <titlejirakul@gmail.com>
2021-03-24 09:56:53 -0700

    Added day pass model

---
+++ server/models/dayPass-model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const DayPasses = new Schema(
+    {
+        reservationNo: { type: String, required: true },
+        dateBooked: { type: String, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('dayPasses', DayPasses)
