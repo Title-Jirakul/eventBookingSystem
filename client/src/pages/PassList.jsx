cb5f970c1a47cf04543e52392239636df1278470
watsapol.jirakul <watsapol.jirakul@accenture.com>
2025-12-09 15:14:35 +0700

    Update tables

--- client/src/pages/PassList.jsx
+++ client/src/pages/PassList.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-// import ReactTable from 'react-table-v6'
 import api from '../api'
 import { AdminNavBar } from '../components'
 
+import { DataGrid } from '@mui/x-data-grid'
 import styled from 'styled-components'
-// import 'react-table-v6/react-table.css'
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
@@ -24,21 +23,16 @@ class DeletePass extends Component {
     deleteUser = async event => {
         event.preventDefault()
 
-        if (
-            window.confirm(
-                `Do you want to delete the ticket ${this.props.reservationNo} permanently?`,
-            )
-        ) {
-            await api.deletePass(this.props.id).then(() => {
-               api.deleteSinglePass(this.props.id).then(() => {
-                  window.location.reload()
-               })
-               api.deleteDayPass(this.props.id).then(() => {
-                  window.location.reload()
-               })
-            }).catch(() => {
-               window.location.reload()
-            })
+        if (window.confirm(
+            `Do you want to delete the ticket ${this.props.reservationNo} permanently?`
+        )) {
+            try {
+                await api.deletePass(this.props.id)
+                await api.deleteSinglePass(this.props.id)
+                await api.deleteDayPass(this.props.id)
+            } finally {
+                window.location.reload()
+            }
         }
     }
 
@@ -50,7 +44,6 @@ class DeletePass extends Component {
 class UpdatePass extends Component {
     updateUser = event => {
         event.preventDefault()
-
         window.location.href = `/passes/update/${this.props.id}`
     }
 
@@ -64,110 +57,77 @@ class PassList extends Component {
         super(props)
         this.state = {
             passes: [],
-            columns: [],
             isLoading: false,
         }
     }
 
-    componentDidMount = async () => {
+    async componentDidMount() {
         this.setState({ isLoading: true })
 
-        await api.getPasses().then(passes => {
-            const passData = passes.data.data
-            passData.map(data => {
-               data.isActive = data.isActive ? "Yes" : "No"
-            })
-            this.setState({
-                passes: passData,
-                isLoading: false,
-            })
+        const res = await api.getPasses()
+        const passData = res.data.data.map((p) => ({
+            id: p._id,                // MUI DataGrid requires "id"
+            ...p,
+            isActive: p.isActive ? "Yes" : "No",
+        }))
+
+        this.setState({
+            passes: passData,
+            isLoading: false,
         })
     }
 
-    filterMethod = (filter, row) => {
-        const id = filter.pivotId || filter.id
-        return row[id] !== undefined ? row[id].toLowerCase().startsWith(filter.value.toLowerCase()) : true
+    render() {
+        const { passes, isLoading } = this.state
+
+        const columns = [
+            { field: 'reservationNo', headerName: 'Reservation No', flex: 1 },
+            { field: 'name', headerName: 'Name', flex: 1 },
+            { field: 'passType', headerName: 'Ticket Type', flex: 1 },
+            { field: 'phoneNo', headerName: 'Phone Number', flex: 1 },
+            { field: 'isActive', headerName: 'Is Active', flex: 1 },
+
+            {
+                field: 'delete',
+                headerName: '',
+                width: 120,
+                sortable: false,
+                filterable: false,
+                renderCell: (params) =>
+                    <DeletePass id={params.row.id} reservationNo={params.row.reservationNo} />
+            },
+
+            {
+                field: 'update',
+                headerName: '',
+                width: 120,
+                sortable: false,
+                filterable: false,
+                renderCell: (params) =>
+                    <UpdatePass id={params.row.id} />
+            }
+        ]
+
+        return (
+            <Wrapper>
+                <AdminNavBar />
+                <div style={{ height: passes.length * 85 + 150, width: '100%' }}>
+                    <DataGrid
+                        rows={passes}
+                        columns={columns}
+                        loading={isLoading}
+                        disableRowSelectionOnClick
+                        autoHeight
+                        sx={{
+                            backgroundColor: '#fff',
+                            borderRadius: 2,
+                        }}
+                        pageSizeOptions={[passes.length]}
+                    />
+                </div>
+            </Wrapper>
+        )
     }
-
-    // render() {
-    //     const { passes, isLoading } = this.state
-
-    //     const columns = [
-    //         {
-    //             Header: 'Reservation No',
-    //             accessor: 'reservationNo',
-    //             filterable: true,
-    //             filterMethod: this.filterMethod,
-    //         },
-    //         {
-    //             Header: 'Name',
-    //             accessor: 'name',
-    //             filterable: true,
-    //             filterMethod: this.filterMethod,
-    //         },
-    //         {
-    //             Header: 'Ticket Type',
-    //             accessor: 'passType',
-    //             filterable: true,
-    //             filterMethod: this.filterMethod,
-    //         },
-    //         {
-    //             Header: 'Phone Number',
-    //             accessor: 'phoneNo',
-    //             filterable: true,
-    //             filterMethod: this.filterMethod,
-    //         },
-    //         {
-    //             Header: 'Is Active',
-    //             accessor: 'isActive',
-    //             filterable: true,
-    //             filterMethod: this.filterMethod,
-    //         },
-    //         {
-    //             Header: '',
-    //             accessor: '',
-    //             Cell: function(props) {
-    //                 return (
-    //                     <span>
-    //                         <DeletePass id={props.original._id} reservationNo={props.original.reservationNo}/>
-    //                     </span>
-    //                 )
-    //             },
-    //         },
-    //         {
-    //             Header: '',
-    //             accessor: '',
-    //             Cell: function(props) {
-    //                 return (
-    //                     <span>
-    //                         <UpdatePass id={props.original._id} />
-    //                     </span>
-    //                 )
-    //             },
-    //         },
-    //     ]
-
-    //     let showTable = true
-    //     if (!passes.length) {
-    //         showTable = false
-    //     }
-
-    //     return (
-    //         <Wrapper>
-    //             <AdminNavBar/>
-    //             {showTable && (
-    //                 <ReactTable
-    //                     data={passes}
-    //                     columns={columns}
-    //                     loading={isLoading}
-    //                     defaultPageSize={passes.length}
-    //                     showPagination={false}
-    //                     minRows={0}
-    //                 />
-    //             )}
-    //         </Wrapper>
-    //     )
-    // }
 }
 
 export default PassList
