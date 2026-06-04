41ccd27d0c0495c2bd83eaa0524580038f314f5f
Watsapol Jirakul <watsapoljirakul@Watsapols-MacBook-Pro.local>
2026-06-04 13:30:18 +0700

    Update backend api path

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://make-friends-server.herokuapp.com/api',
+    baseURL: 'https://eventbookingsystem-1.onrender.com/api',
     headers: {
         common: {
             'Content-Type': 'application/json'
